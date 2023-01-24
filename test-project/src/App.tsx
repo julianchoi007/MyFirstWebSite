f727ffe01f414fa2d35c97e9284cd4ec850cba7f
julianchoi007 <choijulian007@gmail.com>
2023-01-24 14:36:02 -0500

    Testing emphasis

--- test-project/src/App.tsx
+++ test-project/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <em>Learn React</em>
         </a>
       </header>
     </div>
