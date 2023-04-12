591eb07c98e775b23dbe76c185d5999e5b7c7714
julianchoi007 <choijulian007@gmail.com>
2023-04-11 22:45:23 -0400

    Added some styling and a button to the navbar

--- react-website/src/components/NavBar/NavBar.tsx
+++ react-website/src/components/NavBar/NavBar.tsx
@@ -5,6 +5,10 @@ type NavBarTypes = {
   title: string;
 };
 
+type propsStyledDiv = {
+  alignment: string;
+};
+
 const StyledNavBar = styled.div`
   width: 100vw;
   background-color: black;
@@ -14,13 +18,31 @@ const StyledNavBar = styled.div`
   position: fixed;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  height: 40px;
+`;
+const StyledDivSection = styled.div<propsStyledDiv>`
+  display: flex;
+  flex-grow: 1;
+  flex-basis: 0;
+  justify-content: ${(props) => props.alignment};
+`;
+const StyledButtonMenu = styled.button`
+  display: flex;
+  justify-content: start;
+  margin: 0 10px;
+  width: 50px;
 `;
 
 const NavBar: FC<NavBarTypes> = ({ title }) => (
   <>
     <StyledNavBar>
-      <h2>{title}</h2>
+      <StyledDivSection alignment="start">
+        <StyledButtonMenu>Button</StyledButtonMenu>
+      </StyledDivSection>
+      <StyledDivSection alignment="center">
+        <h2>{title}</h2>
+      </StyledDivSection>
+      <StyledDivSection alignment="end"></StyledDivSection>
     </StyledNavBar>
   </>
 );
